cfe9be88cc2f1add9a81887358a3abbc99d3bd03
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-01-18 21:20:07 +0300

    tests changed

--- src/js/__test__/app.test.js
+++ src/js/__test__/app.test.js
@@ -1,23 +1,33 @@
 import sortHeroes from '../app';
 
-test('sort 1', () => {
-  const array = sortHeroes([
+test('matcher toBe should not work', () => {
+  const array = [
     { name: 'мечник', health: 10 },
     { name: 'маг', health: 100 },
     { name: 'лучник', health: 80 },
+  ];
+
+  const sortedArray = sortHeroes(array);
+
+  expect(sortedArray).not.toBe([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
   ]);
-  expect(array[0].health).toEqual(100);
-  expect(array[1].health).toEqual(80);
-  expect(array[2].health).toEqual(10);
 });
 
-test('sort 2', () => {
-  const array = sortHeroes([
-    { name: 'мечник', health: 16 },
-    { name: 'маг', health: 32 },
-    { name: 'лучник', health: 32 },
+test('matcher toEqual should work', () => {
+  const array = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const sortedArray = sortHeroes(array);
+
+  expect(sortedArray).toEqual([
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
   ]);
-  expect(array[0].health).toEqual(32);
-  expect(array[1].health).toEqual(32);
-  expect(array[2].health).toEqual(16);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,3 @@ export default function sortHeroes(arr) {
   arr.sort((a, b) => (a.health < b.health ? 1 : -1));
   return arr;
 }
-
-sortHeroes([
-  { name: 'мечник', health: 10 },
-  { name: 'маг', health: 100 },
-  { name: 'лучник', health: 80 },
-]);
